a37cd8dd55ef3839a1c9a6101f284875b5c8f6ff
acevedoossiel <acevedoossiel@gmail.com>
2025-05-28 22:06:40 -0600

    probandoo

--- test/db/db_provider_test.dart
+++ test/db/db_provider_test.dart
@@ -5,35 +5,32 @@ import 'package:panes_app/db/db_provider.dart';
 void main() {
   sqfliteFfiInit();
   databaseFactory = databaseFactoryFfi;
-  test(
-    'La base de datos se crea correctamente con las tablas esperadas',
-    () async {
-      final Database db = await DBProvider.db.database;
 
-      final expectedTables = [
-        'unidades',
-        'tipos',
-        'panes',
-        'ingredientes_catalogo',
-        'receta_tipo_pan',
-        'clientes',
-        'pedido_cliente',
-        'pedido_cliente_detalle',
-      ];
+  test('Se ejecuta initDB y se crean todas las tablas', () async {
+    final db =
+        await DBProvider.db.initDB(); // Aquí llamas directamente al método
+    final tablasEsperadas = [
+      'unidades',
+      'tipos',
+      'panes',
+      'ingredientes_catalogo',
+      'receta_tipo_pan',
+      'clientes',
+      'pedido_cliente',
+      'pedido_cliente_detalle',
+    ];
 
-      final result = await db.rawQuery(
-        "SELECT name FROM sqlite_master WHERE type='table'",
+    final List<Map<String, dynamic>> resultado = await db.rawQuery(
+      "SELECT name FROM sqlite_master WHERE type='table'",
+    );
+    final tablasEnBD = resultado.map((e) => e['name'] as String).toList();
+
+    for (final tabla in tablasEsperadas) {
+      expect(
+        tablasEnBD.contains(tabla),
+        isTrue,
+        reason: 'Falta la tabla $tabla',
       );
-
-      final tablesInDb = result.map((row) => row['name'] as String).toList();
-
-      for (var table in expectedTables) {
-        expect(
-          tablesInDb.contains(table),
-          isTrue,
-          reason: 'Falta la tabla $table',
-        );
-      }
-    },
-  );
+    }
+  });
 }
